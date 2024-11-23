340ae1ef54c446703d292c8acc5ecbbd3bafeecb
Александр <sandulalex@MacBook-Pro-Aleksandr-5.local>
2024-11-23 15:14:00 +0300

    split positive and negative cases

--- tests/test_square.py
+++ tests/test_square.py
@@ -4,16 +4,16 @@ from square import area, perimeter
 
 class TestSquare(unittest.TestCase):
 
-    def test_area(self):
+    def test_area_positive(self):
         self.assertEqual(area(4), 16)
+
+    def test_perimeter_positive(self):
+        self.assertEqual(perimeter(4), 16)
+
+    def test_area_negative(self):
         with self.assertRaises(ValueError):
             area(-4)
 
-    def test_perimeter(self):
-        self.assertEqual(perimeter(4), 16)
+    def test_perimeter_negative(self):
         with self.assertRaises(ValueError):
             perimeter(-4)
-
-
-if __name__ == "__main__":
-    unittest.main()
